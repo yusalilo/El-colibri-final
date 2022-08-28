1c8e331bd055a71d5ce36aa9ab3623ff053c95ad
Javic97 <111356022+Javic97@users.noreply.github.com>
2022-08-27 19:31:15 -0500

    Initial commit

---
+++ src/Movdinero.java
@@ -0,0 +1,13 @@
+public class Movdinero {
+
+    private Long id;
+    private String concept;
+    private Float amount;
+    private user user;
+    private Enterprise Enterprise;
+    private date createAt;
+    private date updatedAt;
+
+
+
+}
